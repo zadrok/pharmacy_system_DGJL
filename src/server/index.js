20c24d7537b7b4d1b4c6e45ffbd0e152ec6bd2bb
Gavin Rossiter <rossiter.gavin@gmail.com>
2017-08-30 17:22:39 +1000

    basic express webserver done use "node src/server/index.js" from the project root directory to run use "/lib" prefix to refer to "node_modules" path

---
+++ src/server/index.js
@@ -0,0 +1,19 @@
+var express = require('express')
+var app = express()
+var path = require('path')
+
+// Define the port to run on
+app.set('port', 3000);
+console.log(path.join(__dirname + '/../public/css'));
+
+app.use('/css', express.static(path.join(__dirname + '/../public/css')));
+app.use('/lib', express.static(path.join(__dirname + '/../../node_modules')));
+app.use('/', express.static(path.join(__dirname, '../public/views')));
+
+// app.use('/lib', express.static(path.join(__dirname + '../../node_modules')));
+
+// Listen for requests
+var server = app.listen(app.get('port'), function() {
+  var port = server.address().port;
+  console.log('Magic happens on port ' + port);
+});
