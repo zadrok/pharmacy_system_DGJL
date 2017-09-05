6c57e2b6c3d5849e9f6623229731176e616ca55b
Gavin Rossiter <rossiter.gavin@gmail.com>
2017-09-05 12:47:54 +1000

    Updated some simple routes.  Just load up the request object with the JSON data to send to the backend.

--- src/server/index.js
+++ src/server/index.js
@@ -6,7 +6,7 @@ var path = require('path')
 app.set('port', 3000);
 console.log(path.join(__dirname + '/../public/css'));
 
-app.use('/js', express.static(path.join(__dirname + '/../public/images')));
+app.use('/images', express.static(path.join(__dirname + '/../public/images')));
 app.use('/js', express.static(path.join(__dirname + '/../public/js')));
 app.use('/css', express.static(path.join(__dirname + '/../public/css')));
 app.use('/lib', express.static(path.join(__dirname + '/../../node_modules')));
@@ -20,7 +20,34 @@ var server = app.listen(app.get('port'), function() {
   console.log('Magic happens on port ' + port);
 });
 
+// >>>> demo code
 app.get('/*.json', function (req, res) {
   console.log("pleb")
   res.json("[{pleb:'pleb'}]")
+})
+// <<<< demo code
+
+app.post('/create', function (req, res) {
+  console.log("create record request")
+  // <do create record>()
+  // get <result> from <do create record>()
+  // respond with res.json(<result>)
+})
+app.post('/read', function (req, res) {
+  console.log("read records request")
+  // <do read records>()
+  // get <result> from <do read records>()
+  // respond with res.json(<result>)
+})
+app.post('/update', function (req, res) {
+  console.log("update records request")
+  // <do update records>()
+  // get <result> from <do update records>()
+  // respond with res.json(<result>)
+})
+app.post('/delete', function (req, res) {
+  console.log("delete records request")
+  // <do delete records>()
+  // get <result> from <do delete records>()
+  // respond with res.json(<result>)
 })
