f896d300b45881f0a9af8547c277c95104365ab3
zadrok <lachlan.e.haggart@gmail.com>
2017-09-12 17:17:05 +1000

    finished modals for add, update, delete on stock page

--- src/public/js/stock.js
+++ src/public/js/stock.js
@@ -3,6 +3,8 @@ var app = angular.module("StockApp", []);
 
 app.controller("StockCtrl", function ($scope, $http) {
   $scope.currentStockItem = null;
+  $scope.newStockItem = null;
+
   $http.post('/read')
   .then(
     function (response) {
@@ -19,16 +21,25 @@ app.controller("StockCtrl", function ($scope, $http) {
   $scope.Delete = function(item)
   {
     console.log("Delete " + item.name);
+    $http.post('/delete');
+
+    $scope.SetCurrentStockItem(null);
   };
 
   $scope.Update = function(item)
   {
-    console.log("Update " + item.name);
+    console.log("Update " + item.name + ", Price: " + item.price + ", Quantity: " + item.quantity);
+    $http.post('/update');
+
+    $scope.SetCurrentStockItem(null);
   };
 
   $scope.Add = function()
   {
-    console.log("Add new item");
+    console.log("Add new item " + $scope.newStockItem.name + ", Price: " + $scope.newStockItem.price + ", Quantity: " + $scope.newStockItem.quantity);
+    $http.post('/create');
+
+    $scope.newStockItem = null;
   };
 
   $scope.SetCurrentStockItem = function(item)
