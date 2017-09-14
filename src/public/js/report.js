6ad62a9611675e361991ba9ec9ba27685ad0077c
zadrok <lachlan.e.haggart@gmail.com>
2017-09-14 16:22:00 +1000

    added button to create csv file

--- src/public/js/report.js
+++ src/public/js/report.js
@@ -3,17 +3,9 @@ var app = angular.module("ReportApp", []);
 
 app.controller("ReportCtrl", function ($scope, $http) {
 
-  $http.post('/read')
-  .then(
-    function (response) {
-      //console.log(response);
-      $scope.orders = response.data;
-
-    //console.log($scope.stock.length);
-    },
-    function (response) {
-      // error handling routine
-    }
-  );
+  $scope.GenerateReport = function()
+  {
+    console.log("Making Report");
+  }
 
 });
