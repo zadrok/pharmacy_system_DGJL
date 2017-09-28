cc6fafba913553d2ba7a5df03914915f6db0dc20
Gavin Rossiter <rossiter.gavin@gmail.com>
2017-09-29 00:09:49 +1000

    Report now displays sales.  Is hardcoded for sku=2, need an input field and binding.

--- src/public/js/report.js
+++ src/public/js/report.js
@@ -2,35 +2,41 @@
 var app = angular.module("ReportApp", []);
 
 app.controller("ReportCtrl", function ($scope, $http) {
-  $scope.dateFrom = new Date(1970,1,1);
+  $scope.dateFrom = new Date();
   $scope.dateTo = new Date();
 
   $scope.GenerateReport = function()
   {
     $scope.requestBody = JSON.stringify({dateFrom:$scope.dateFrom.toJSON(), dateTo:$scope.dateTo.toJSON()});
-    $http.post('/read-sales-for-report', $scope.requestBody)
+    $http.post('/read-sales', $scope.requestBody)
     .then(
       function (response) {
         $scope.reportData = response.data;
+        let sku = 2; // Need an input field for this guy like in sales page
+        let skuData = $scope.reportData.filter((record) => record.sku == sku)
+        let time = $scope.reportData.map((record) => new Date(record.date) )
+        let sales = skuData.map((record) => record.quantity )
+        let salesValue = skuData.map((record) => Number(record.quantity)*Number(record.price) )
         // reportData should contain a property that replaces x and data1 below
         console.log("Making Report");
-        c3.generate({
+        let chart = c3.generate({
           bindto: '#chart',
           data: {
-            x: 'x',
+            x: 'time',
+            xFormat: '%Y-%m-%dT%H:%M:%S.%LZ',
             columns: [
-              ['x', '2013-01-01', '2013-01-02', '2013-01-03', '2013-01-04', '2013-01-05', '2013-01-06'],
-              ['data1', 30, 200, 100, 400, 150, 250],
-              ['data2', 50, 20, 10, 40, 15, 25]
+              ['time'].concat(time),
+              ['sales'].concat(sales),
+              ['salesValue'].concat(salesValue)
             ]
           },
           axis: {
-            x: {
-              type: 'timeseries',
-              tick: {
-                  format: '%Y-%m-%d'
+              x: {
+                  type: 'timeseries',
+                  tick: {
+                      format: '%Y-%m-%d %H:%M:%S',
+                  }
               }
-            }
           }
         })
       }
